e3808fe1a691da03500fbfe38c6ed5a33afad533
volkmaster <ziga.vucko@gmail.com>
2017-05-23 20:09:22 +0200

    Name fix.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Sockets
+                    ZANS
                 </div>
 
                 <div class="links">

f3ebad40e7d0ccce0a4219e54bfcc3503161534f
tinaavbelj <tina.avbelj@gmail.com>
2017-07-08 19:17:11 +0200

    dummy commit

--- public/js/app.js
+++ public/js/app.js
@@ -29432,7 +29432,7 @@ module.exports = function normalizeComponent (
 /***/ (function(module, exports, __webpack_require__) {
 
 module.exports={render:function (){var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;
-  return _c('div', [_vm._v("\n    ABC\n")])
+  return _c('div', [_vm._v("\n    xyz\n")])
 },staticRenderFns: []}
 module.exports.render._withStripped = true
 if (false) {
--- resources/assets/js/components/App.vue
+++ resources/assets/js/components/App.vue
@@ -4,7 +4,7 @@
 
 <template>
     <div>
-        ABC
+        xyz
     </div>
 </template>
 

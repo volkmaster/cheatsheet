af891eaed3a844e16e76dffdf7a9179024ba29be
volkmaster <ziga.vucko@gmail.com>
2017-07-29 17:18:59 +0200

    basic component system finished, router added

---
+++ resources/assets/js/components/NavFooter.vue
@@ -0,0 +1,23 @@
+<style lang="scss" scoped>
+@import '../../sass/app';
+
+.footer-wrapper {
+    @if ($debug) { border: 1px solid green; }
+
+    width            : 100%;
+    height           : 150px;
+    background-color : $text-color;
+}
+</style>
+
+<template>
+    <div class="footer-wrapper"></div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {}
+    }
+}
+</script>

38aa9e57d4541a0bf37d48934ad9e0a4f6f497d5
volkmaster <ziga.vucko@gmail.com>
2017-08-12 03:04:17 +0200

    axios base url fix

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -1,5 +1,3 @@
-import * as constants from './utils/constants'
-
 window._ = require('lodash')
 
 window.$ = window.jQuery = require('jquery')
@@ -7,7 +5,6 @@ window.$ = window.jQuery = require('jquery')
 window.Vue = require('vue')
 
 window.axios = require('axios')
-window.axios.defaults.baseURL = constants.BASE_URL
 window.axios.defaults.headers.common = {
     'Accept': 'application/json',
     'X-CSRF-TOKEN': window.Laravel.csrfToken,
